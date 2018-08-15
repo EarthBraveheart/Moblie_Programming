fc7216dc0de89dfc4eb0cea990e5155ca053f5f1
EarthBraveheart <59070086@it.kmitl.ac.th>
2018-08-15 11:56:02 +0700

    commit

---
+++ app/src/main/java/com/example/lab203_38/myuser/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab203_38.myuser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flagment_login);
+    }
+}
